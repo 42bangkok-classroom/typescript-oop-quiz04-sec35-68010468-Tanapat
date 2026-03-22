c4d06ea0f32a714f843a0229f948c046a1fb87e7
68010468-Tanapat <68010468@kmitl.ac.th>
2026-03-22 16:02:21 +0700

    9

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get , Param , Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService : UserService) {}
 
-  @Get()
+  @Get('test')
   gettest() {
     return this.userService.test();
+  }
+  users(){
+    return this.userService.findAll();
+  }
+  @Get(':id')
+  findOne(
+    @Param('id') id: string, 
+    @Query('fields') fields?: string
+  ) {
+    // ถ้ามีการส่ง fields มา (เช่น fields=firstName,lastName) ให้แยก string ด้วยเครื่องหมายลูกน้ำเป็น Array
+    const fieldsArray = fields ? fields.split(',') : undefined;
+    
+    // เรียกใช้ service
+    return this.userService.findOne(id, fieldsArray);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { IUser } from './user.interface';
 
 @Injectable()
 export class UserService {
   test() {
-    return [];
+    return [] ;
   }
+  async findAll(){
+    
+  }
+  findOne(id: string, fields?: string[]) {
+    // 1. อ่านข้อมูลจากไฟล์ (ถ้าคุณมี this.findAll() จากข้อ 3 แล้ว สามารถเรียกใช้แทน 2 บรรทัดนี้ได้เลย)
+    const filePath = path.join(process.cwd(), 'data', 'users.json');
+    const users: IUser[] = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+    // 2. ค้นหา user ตาม id
+    const user = users.find((u) => u.id === id);
+
+    // 3. ถ้าไม่พบ user ให้ throw NotFoundException (404)
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    // 4. ถ้ามี Query Parameter fields ระบุมา ให้ทำการ Filter Fields
+    if (fields && fields.length > 0) {
+      const filteredUser = {};
+      fields.forEach((field) => {
+        // เช็คว่ามี field นี้ใน object user หรือไม่
+        if (user[field as keyof IUser] !== undefined) {
+          filteredUser[field] = user[field as keyof IUser];
+        }
+      });
+      return filteredUser;
+    }
+    return user;
+}
 }
